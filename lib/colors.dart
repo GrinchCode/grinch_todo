8c5410c1a24a7f88ebb59cd7f8b2d90e89ce4574
Frank Moreno <frankmoreno1993@gmail.com>
2020-05-15 21:59:17 -0500

    Actualizar paleta y tema

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-var primaryColor =  Color(0xff133049);
+class GrinchColors {
+  const GrinchColors._();
+
+  static const blueDark = Color(0xff133049);
+
+  static const green = Color(0xff34b49f);
+}
+
+class GrinchTheme {
+  static const primaryColor = GrinchColors.blueDark;
+  static const accentColor = GrinchColors.green;
+}
